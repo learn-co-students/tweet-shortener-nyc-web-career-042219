5b58714bc2859c4cf4d674b888a46facc21dea1e
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-17 22:03:43 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,6 +5,7 @@ def dictionary
     "two" => '2',
     "too" => '2',
     "for" => '4',
+    "For" => '4',
     "four" => '4',
     'be' => 'b',
     'you' => 'u',
@@ -24,3 +25,25 @@ def word_substituter(tweet)
  end  
  words.join(" ")
  end
+ 
+ def bulk_tweet_shortener(tweet_array)
+   tweet_array.collect do |tweet|
+     puts word_substituter(tweet)
+   end
+ end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet 
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    tweet[0...140]
+  else 
+    word_substituter(tweet)
+end
+end
