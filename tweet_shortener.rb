fce81ffced3703c66974eaf2ef45b8e5613864bc
Augustus Kirby <kirbyaugustus@gmail.com>
2019-04-19 15:47:23 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,20 +1,52 @@
-def dictionary
+require "pry"
 
-  dictionary = {
-  hello: 'hi',
-  to: '2',
-  two: '2',
-  too: '2',
-  for: "4",
-  four: '4',
-  be:  'b',
-  you:  'u',
-  at:  "@",
-  and:  "&"
+
+def dictionary
+{
+"hello" => "hi",
+"to" => "2",
+"two" => "2",
+"too" => "2",
+"for" => "4",
+"four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@",
+"and" => "&"
 }
 end
 
-def word_substituterO(tweet)
-  tweet.split.map do |word|
-    word.
-  end
+# binding.pry
+
+def word_substituter(tweet)
+  output = []
+  tweet.split.each do |x|
+   if dictionary.keys.include?(x.downcase)
+     x = dictionary[x.downcase]
+    output << x
+   else
+     output << x
+   end
+ end
+ output.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+  # tweet = array.join(".")
+  puts word_substituter(tweet)
+end
+end
+
+
+def selective_tweet_shortener(tweet)
+  tweeter = tweet.split(" ")
+  if tweeter.length > 140
+    bulk_tweet_shortener(tweeter)
+  else
+    tweet
+end
+end
+
+def shortened_tweet_truncator(tweet)
+end
